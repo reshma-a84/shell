23ef1c0d5efeeb3e8d69354fe1c02ea7684861d8
reshma-a84 <reshu.a84@gmail.com>
2023-03-22 21:42:19 +0530

    March 22 2023 final for Day1

--- 1simplebash.sh
+++ 1simplebash.sh
@@ -1,3 +1,41 @@
 #!/bin/bash
 
+#This script displays various information to the screen
+
+# Display 'Hello, world'
 echo "Hello, world!"
+
+# Assign a value to a variable
+WORD='script'
+
+# Display that value using the variable 
+echo "$WORD"    
+
+# Demonstrate that single quotes cause variables to NOT get expanded
+echo '$WORD'
+
+# Combine the variable with hard-coded text
+echo "This is a shell $WORD"
+
+# Display the contents of the variable using an alternative syntax
+echo "This is a shell ${WORD}"
+
+# Append text to the variable
+echo "${WORD}ing is fun!"
+
+# How NOT to append text to a variable
+echo "$WORDing is fun!"
+
+# Create a new variable
+ENDING='ed'
+
+# Combine the two variables
+echo "This is ${WORD}${ENDING}"
+
+# Change the value stored in the ENDING variable aka REASSIGNMENT
+ENDING='ing'
+echo "${WORD}${ENDING} is fun!"
+
+# Reassign value to ENDING
+ENDING='s'
+echo "You are going to write many ${WORD}${ENDING} in this file!!!!" 
